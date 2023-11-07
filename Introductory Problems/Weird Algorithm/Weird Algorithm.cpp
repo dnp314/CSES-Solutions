a744006a4bd8c623297fb599322a9e70ce957393
Divyesh Parikh <119059430+dnp314@users.noreply.github.com>
2023-11-07 23:12:49 +0530

    Update Weird Algorithm.cpp

--- Introductory Problems/Weird Algorithm/Weird Algorithm.cpp
+++ Introductory Problems/Weird Algorithm/Weird Algorithm.cpp
@@ -1,1 +1,41 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+const ll MOD = 1e9 + 7;
+#define fo(i,a,b) for(int i=a;i<b;i++)
+ 
+void solve() {
+ll n;
+cin>>n;
+vector<ll>ans;
+while(n!=1)
+{
+    if(n%2==0)
+    {
+        ans.push_back(n);
+        n/=2;
+    }
+    else 
+    {
+        ans.push_back(n);
+        n=(n*3)+1;
+    }
+ 
+}
+ans.push_back(1);
+for(auto it:ans)
+cout<<it<<" ";
+ 
+}
+ 
+int main()
+{
+ios_base::sync_with_stdio(0);
+cin.tie(0); cout.tie(0);
+int tc = 1;
+// cin >> tc;
+while(tc--){
+solve();
+}
+    return 0;
+}
